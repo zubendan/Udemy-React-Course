6d62dd2270680c73b7eb8b8ff18fc331e12bd0eb
Daniel <daniel.zubenko@gmail.com>
2021-07-12 23:42:11 -0700

    made first component

---
+++ my-app/src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+/// expense tracker 
+const ExpenseItem = () => {
+    return (
+        <div>
+            <h2>
+             Expense item!
+            </h2>
+        </div>
+    );
+};
+
+export default ExpenseItem;

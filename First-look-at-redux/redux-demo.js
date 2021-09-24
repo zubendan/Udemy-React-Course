cf91a4caf2099a4b95957cd0387c5400d62f9d07
Daniel <daniel.zubenko@gmail.com>
2021-09-23 21:59:51 -0700

    demo complete

---
+++ First-look-at-redux/redux-demo.js
@@ -0,0 +1,28 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      couter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });

9a812f7901c72ddd7afb2dab4a166690ff281457
Daniel <daniel.zubenko@gmail.com>
2021-10-10 21:57:42 -0700

    mind =  blown

--- Replacing-redux/src/index.js
+++ Replacing-redux/src/index.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import "./index.css";
+import App from "./App";
+import configureStore from "./hooks-store/products-store";
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
+configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  // <ProductsProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  // </ProductsProvider>
+  document.getElementById("root")
 );
